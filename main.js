af56b921b11c7c682753162f9cc34e31ae428c2c
oscaroviedo-png <oscar.oviedo@mail.udp.cl>
2026-09-25 00:28:54 -0300

    Función en JavaScript de agregar cultivo

--- main.js
+++ main.js
@@ -35,3 +35,68 @@ if (ListaDeCultivos && BotonDeFiltro) {
 
 }
 
+
+//Función de agregar cultivo abajito...
+const BotonDeAgregar = document.getElementById("Boton_De_Agregar");
+
+if (BotonDeAgregar && ListaDeCultivos) {
+
+    BotonDeAgregar.addEventListener("click", function () {
+
+        const Nombre = prompt("Ingrese el nombre del cultivo:");
+
+        if (!Nombre) {
+            return;
+        }
+
+        const Tipo = prompt("Ingrese el tipo del cultivo:");
+
+        if (!Tipo) {
+            return;
+        }
+
+        const Estado = prompt("Ingrese el estado: Activo, En Desarrollo, Experimental o Finalizado");
+
+        if (!Estado) {
+            return;
+        }
+
+        const Cuarentena = prompt("Ingrese el nivel de cuarentena (de 0 a 10):");
+
+        if (!Cuarentena) {
+            return;
+        }
+
+        const Fecha = prompt("Ingrese la fecha de inicio (formato dd/mm/aaaa):");
+
+        if (!Fecha) {
+            return;
+        }
+
+        const Fila = document.createElement("tr");
+
+        const NombreCelda = Fila.insertCell();
+        NombreCelda.textContent = Nombre;
+
+        const TipoCelda = Fila.insertCell();
+        TipoCelda.textContent = Tipo;
+
+        const EstadoCelda = Fila.insertCell();
+        EstadoCelda.textContent = Estado;
+
+        const CuarentenaCelda = Fila.insertCell();
+        CuarentenaCelda.textContent = Cuarentena + "/10";
+
+        const FechaC = Fila.insertCell();
+        FechaC.textContent = Fecha;
+
+
+        const Acciones = Fila.insertCell();
+
+        Acciones.innerHTML = `<button type="button" class="Boton_De_Editar">Modificar</button>
+            <button type="button" class="Boton_De_Eliminar">Purgar</button>`;
+        ListaDeCultivos.appendChild(Fila);
+
+    });
+
+}
